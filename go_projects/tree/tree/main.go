089f716f92a90398ba56c5ada9f2043682a3bdc0
dimbo1324 <dimaprihodko180@gmail.com>
2026-01-15 16:14:03 +0300

    end

--- go_projects/tree/tree/main.go
+++ go_projects/tree/tree/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"errors"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"sort"
 )
 
 const (
@@ -31,6 +33,73 @@ func main() {
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	// TODO: ваша реализация
-	return errors.New("не реализовано")
+	return walk(out, path, printFiles, "")
+}
+
+func walk(out io.Writer, path string, printFiles bool, prefix string) error {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	var filtered []os.DirEntry
+	for _, entry := range entries {
+		if entry.Name() == ".DS_Store" {
+			continue
+		}
+		if !printFiles && !entry.IsDir() {
+			continue
+		}
+		filtered = append(filtered, entry)
+	}
+
+	sort.Slice(filtered, func(i, j int) bool {
+		return filtered[i].Name() < filtered[j].Name()
+	})
+
+	for i, entry := range filtered {
+		isLast := i == len(filtered)-1
+
+		var connector string
+		if isLast {
+			connector = endSign
+		} else {
+			connector = dirSign
+		}
+
+		var sizeInfo string
+		if !entry.IsDir() {
+			info, err := entry.Info()
+			if err != nil {
+				return err
+			}
+			if info.Size() == 0 {
+				sizeInfo = " (empty)"
+			} else {
+				sizeInfo = fmt.Sprintf(" (%db)", info.Size())
+			}
+		}
+
+		_, err := fmt.Fprintf(out, "%s%s%s%s\n", prefix, connector, entry.Name(), sizeInfo)
+		if err != nil {
+			return err
+		}
+
+		if entry.IsDir() {
+			newPrefix := prefix
+			if isLast {
+				newPrefix += "\t"
+			} else {
+				newPrefix += pipeSign
+			}
+
+			subPath := filepath.Join(path, entry.Name())
+
+			err := walk(out, subPath, printFiles, newPrefix)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
